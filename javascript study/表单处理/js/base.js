c1dd36350689c08978283055c0082098295af1dc
BadworkerN <381900623@qq.com>
2016-12-04 21:32:35 +0800

    web

---
+++ javascript study/表单处理/js/base.js
@@ -0,0 +1,24 @@
+function addEvent(obj, evt, fn){
+	if(obj,addEventListener){
+		obj.addEventListener(evt, fn, false);
+	}else if(obj.attachEvent){
+		obj.attachEvent('on'+evt, fn);
+	}
+}
+
+function preDef(evt){
+	var e = evt || window.event;
+	if(e.preventDefault)
+		e.preventDefault();
+	else
+		e.returnValue = false;		
+}
+		
+
+function getCharCode(evt){
+	var e = evt || window.event;
+	if(typeof e.charCode == 'number'){
+		return e.charCode;
+	}else
+		return e.keyCode;
+}
